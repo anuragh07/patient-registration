f6e60542f9561d3923c8c47a10d8f503c1a26c0e
Anuragh <kamatamprem@gmail.com>
2025-05-24 02:00:48 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [react()],
   optimizeDeps: {
     // This is the crucial part for PGLite
